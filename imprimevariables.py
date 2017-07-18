040fcfaefb057fbc8593ac05e6cd5f9f0ec133c0
sophia <gigi.romero560@gmail.com>
2017-07-18 14:43:40 -0500

    Ejercicios de Omega terminados ver 1

---
+++ imprimevariables.py
@@ -0,0 +1,6 @@
+def imprime():
+    print 10
+    print 3.1416
+
+if __name__ == '__main__':
+    imprime()
---
+++ promedioDECIMALES.py
@@ -0,0 +1,19 @@
+def promedio ():
+    cal = int(raw_input())
+    for i in cal:
+        a = int(raw_input())
+        list[i] = a
+
+    for n in list:
+        b = list()
+        b += b
+    
+    x = b/cal
+    
+    return x 
+        
+    
+if __name__ == '__main__':
+    promedio()
+    
+    print x 
